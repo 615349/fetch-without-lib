4edc34186cec303c32eacd7b6fdeab086958726a
Ben Li <ben.li@qantas.com.au>
2019-04-11 15:26:33 +1000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+  state = {
+    hasError: false,
+    coins: []
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
+    const { coins, hasError } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <h3>coins:</h3>
+        {
+          !hasError && coins.map(coin => (
+            <div className='coin-container' key={coin.id}>
+              <div className="name">{coin.name}</div>
+              <div className="price">{coin.price_usd}</div>
+            </div>
+          ))
+        }
+        {
+          hasError && <h2 className='error-message'>Something is wrong</h2>
+        }
+      </>
     );
+  }
+
+  fetchData() {
+    return fetch('https://api.coinmarketcap.com/v1/ticker/?limit=10')
+          .then(response => {
+            if(!response.ok) {
+              throw new Error('Failed to fetch');
+            }
+            return response.json();
+          })
+          .then(coins => {
+            this.setState({ coins, hasError: false })
+          })
+          .catch((e) => {
+            console.error(e);
+            this.setState({ hasError: true })
+          })
   }
 }
 
